9b7e9577cbd4878e3d88075c604c57579ab025c1
Ryan B <admirallasers@gmail.com>
2016-10-22 13:48:42 -0500

    to phase 2!

--- FetchNStore/Scripts/Controllers/formctrl.js
+++ FetchNStore/Scripts/Controllers/formctrl.js
@@ -1,4 +1,4 @@
-﻿app.controller("FormCtrl", function ($scope, $location) {
+﻿app.controller("FormCtrl", function ($scope,$http) {
 
     $scope.url = "";
 
@@ -12,7 +12,17 @@
 
     $scope.MakeRequest = () => 
     {
-        console.log($scope.url, $scope.method);
+        $http({
+            method: $scope.method,
+            url: $scope.url,
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+        },
+        }).then(function successCallback(response) {
+            console.log(response);
+        }, function errorCallback(response) {
+            console.log("error");
+        });
     }
 
 });
