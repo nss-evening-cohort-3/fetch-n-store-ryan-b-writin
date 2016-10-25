2394ef82b36ccc871cc34dff102f4a377e1bc963
Ryan B <admirallasers@gmail.com>
2016-10-24 20:16:50 -0500

    now tracks response time

--- FetchNStore/Scripts/Controllers/formctrl.js
+++ FetchNStore/Scripts/Controllers/formctrl.js
@@ -1,10 +1,10 @@
-﻿app.controller("FormCtrl", function ($scope,$http) {
+﻿app.controller("FormCtrl", function ($scope, $http) {
 
     $scope.url = "";
 
     $scope.method = "";
 
-    $scope.response = {
+    var newResponse = {
         url: "",
         method: "",
         responsetime: ""
@@ -12,14 +12,19 @@
 
     $scope.MakeRequest = () => 
     {
+        var sendDate = (new Date()).getTime();
         $http({
             method: $scope.method,
             url: $scope.url,
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
-        },
+            },
         }).then(function successCallback(response) {
             console.log(response);
+            var receiveDate = (new Date()).getTime();
+            var responseTimeInMs = receiveDate - sendDate;
+            $('#outputTarget').append("<br>URL: "+ $scope.url+" status: " + response.status + " response time: " + responseTimeInMs);
+
         }, function errorCallback(response) {
             console.log("error");
         });
